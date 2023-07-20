e80afa9b26812bad95604d4e620c85380810b32c
Batuhan Göktaş <mbg.is.king@gmail.com>
2023-07-20 12:52:19 +0300

    step-8

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -60,13 +60,24 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                 await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: email, password: password);
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  //users email is verified
                 Navigator.of(context).pushNamedAndRemoveUntil(
                   notesRoute,
                   (route) => false,
                 );
+                } else {
+                  //users email is NOT verified
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                  verifyEmailRoute,
+                  (route) => false,
+                );
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
                   await showErrorDialog(context, "User not found");
@@ -95,5 +106,3 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 }
-
-
